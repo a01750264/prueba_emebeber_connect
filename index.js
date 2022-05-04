ff7fb441622982c49387de77597813cbf95ff7e1
a01750264 <a01750246@itesm.mx>
2022-05-04 18:01:24 -0500

    cambios

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/principal", (req, res) => {
   console.log("Entre");
-  res.sendFile(path.join(__dirname, "..", "prueba.html"));
+  res.sendFile("/home/ec2-user/prueba_emebeber_connect/prueba.html");
 });
 
 //No es necesario que tengan tanto el protocolo http y https funcionando al mismo tiempo
